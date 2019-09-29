b319821639c659332207177fda689b5af4cfa9b0
Joppe Boekestijn <joppeboekestijn@gmail.com>
2019-09-29 14:19:12 +0200

    working alpha version

--- simulation/scheduler.py
+++ simulation/scheduler.py
@@ -1,3 +1,5 @@
+import random
+import model
 from collections import OrderedDict
 
 
@@ -12,7 +14,7 @@ class Scheduler():
     def add(self, agent):
         self.agents[agent.pos] = agent
 
-    # Add agent to OrderedDict
+    # Remove agent to OrderedDict
     def remove(self, agent):
         del self.agents[agent.pos]
 
@@ -26,3 +28,34 @@ class Scheduler():
             # Steps are not used yet
             self.steps += 1
             agent.step()
+
+        # Let adults reproduce with a certain percentage
+        # NOTE: Rather not have this in this step function, but cannot mutate
+        # dictionary while iterating over it (if we put this function in Agent class)
+        counter = 0
+        for i in range(sum(agent.type == 1 for agent in self.agents.values())):
+            if random.random() <= self.model.reproduction:
+                counter += 1
+                # Create agents, place them on the grid and add them to the scheduler
+                agent = model.Agent((None, None), self.model, agent_type=0, age=0)
+                self.model.grid.place_agent_on_empty(agent)
+                self.add(agent)
+
+        # Print summary of numbers of agents in the agent groups as well as number of new agents
+        print("Number of happy agents: ", self.model.happy)
+        print("Number of unhappy agents: ", len(self.agents) - self.model.happy)
+        print("Number of student agents: ", sum(agent.type == 0 for agent in self.agents.values()))
+        print("Number of adults agents: ", sum(agent.type == 1 for agent in self.agents.values()))
+        print("Number of elderly agents: ", sum(agent.type == 2 for agent in self.agents.values()))
+        print("Number of new agents: ", counter)
+
+        # Remove agents that are too old from grid
+        agents_removed = {pos: agent for pos, agent in self.agents.items() if agent.destroy is True}
+        for pos, agent in agents_removed.items():
+            self.model.grid.remove_agent(agent)
+
+        # Remove agents that are too old from the agents dictionary
+        self.agents = {pos: agent for pos, agent in self.agents.items() if agent.destroy is False}
+
+        if not self.agents:
+            self.model.running = False
--- simulation/visualization.py
+++ simulation/visualization.py
@@ -1,12 +1,13 @@
-from model import Model
+import sys
 import tkinter as tk
+from model import Model
 
 
 class Visualization():
     def __init__(self, model):
         # Initialize model
         self.model = model
-        #self.root.after(20,self.render)
+        # self.root.after(20,self.render)
         self.root = tk.Tk()
         self.text_print_arr = []
         self.render()
@@ -14,7 +15,6 @@ class Visualization():
     def step(self):
         self.model.step()
         self.render()
-        #self.render()
 
     # Print ascii text of 2D grid
     def render(self):
@@ -28,8 +28,10 @@ class Visualization():
                     text += 'X'
                 elif c.type == 1:
                     text += '0'
+                elif c.type == 2:
+                    text += '#'
                 else:
-                    text += '+'
+                    text += ' '
                 # c = self.model.grid[y][x]
                 # if c is None:
                 #     tk.Label(root,text=" ", relief=tk.RIDGE, width=15).grid(row=y,column=x)
@@ -45,19 +47,18 @@ class Visualization():
 
             text += '\n'
         self.text_print_arr.append(text)
-        #print(text)
+        # print(text)
 
     def print_text_grid(self):
 
-        #to perform the first iteration
+        # to perform the first iteration
         self.text_gui(0)
-        #to update the same window again and again
+        # to update the same window again and again
         self.root.after(3000, self.text_gui,1)
-        #to destroy finally
-        #self.root.after(1000, lambda: self.root.destroy())
+        # to destroy finally
+        # self.root.after(1000, lambda: self.root.destroy())
 
-
-    def text_gui(self,each_text_grid_itr):
+    def text_gui(self, each_text_grid_itr):
 
         if each_text_grid_itr == 0:
             each_text_grid_print = self.text_print_arr[each_text_grid_itr]
@@ -72,11 +73,8 @@ class Visualization():
                     elif each_text[each_col] == '0':
                         tk.Label(self.root,text="O", relief=tk.RIDGE, width=15, fg="green").grid(row=each_row,column=each_col)
                     else:
-                        tk.Label(self.root,text="+", relief=tk.RIDGE, width=15, fg="red").grid(row=each_row,column=each_col)
-
-
+                        tk.Label(self.root,text="#", relief=tk.RIDGE, width=15, fg="blue").grid(row=each_row,column=each_col)
         elif each_text_grid_itr < len(self.text_print_arr):
-
             each_text_grid_print = self.text_print_arr[each_text_grid_itr]
             each_text_grid_row_split = each_text_grid_print.split('\n')
             for each_row in range(len(each_text_grid_row_split)):
@@ -89,46 +87,60 @@ class Visualization():
                     elif each_text[each_col] == '0':
                         tk.Label(self.root,text="O", relief=tk.RIDGE, width=15, fg="green").grid(row=each_row,column=each_col)
                     else:
-                        tk.Label(self.root,text="+", relief=tk.RIDGE, width=15, fg="red").grid(row=each_row,column=each_col)
+                        tk.Label(self.root,text="#", relief=tk.RIDGE, width=15, fg="blue").grid(row=each_row,column=each_col)
 
-        
             each_text_grid_itr = each_text_grid_itr + 1
-            self.root.after(1000,self.text_gui,each_text_grid_itr)
+            self.root.after(1000, self.text_gui,each_text_grid_itr)
 
         elif each_text_grid_itr == len(self.text_print_arr):
-            #self.root.after(1000, lambda: self.root.destroy())
+            # self.root.after(1000, lambda: self.root.destroy())
             each_text_grid_itr = each_text_grid_itr + 1
-
-
 
 
 # Initialize input parameters of model
 if __name__ == '__main__':
+    print("Default parameters:")
+    print("     height/width:   10")
+    print("     density:        0.8")
+    print("     homophily:      2")
+    print("     ageing:         3")
+    print("     reproduction:   0.5")
+    default = input("Do you want default parameters? [y/n]: ")
+    if default == 'n':
+        dim = input("Enter dimensions of the grid (default = 10): ")
+        density = input("Enter percentage of density (default = 0.8): ")
+        homophily = input("Enter number of neighbors agent requires to be happy (default = 2): ")
+        ageing = input("Enter number of epochs it takes for agent to advance to next group (default = 3): ")
+        reproduction = input("Enter percentage of reproducibility for adults (default = 0.5): ")
+    else:
+        # Else default parameters
+        dim, density, homophily, ageing, reproduction = 10, 0.8, 2, 3, 0.5
+
     model_params = {
-        "height": 10,
-        "width": 10,
-        # Agent density, from 0.8 to 1.0
-        "density": 0.8,
-        # Homophily, from 2 to 8
-        "homophily": 2
+        "height": int(dim),
+        "width": int(dim),
+        "density": float(density),
+        "homophily": int(homophily),
+        "ageing": int(ageing),
+        "reproduction": float(reproduction)
     }
 
     model = Model(**model_params)
     viz = Visualization(model)
 
     # Run the model for 100 epochs
-    for i in range(100):
+    for i in range(20):
         if model.running:
-            print("Step:", i)
+            print("Step:", i + 1)
             viz.step()
             print('----')
 
-    
+    print("In the simulation the places on the grid represent locations an agent can live")
+    print("there are three different groups of agents, namely; students(X), adults(O) and elderly(#)")
+    print("Each iteration the agents grow and if unhappy move to a random location, until they are all happy")
+    print("or the simulation has run 20 epochs")
+
     viz.print_text_grid()
-    #viz.root.after(1000, lambda: viz.root.destroy())
+    # viz.root.after(1000, lambda: viz.root.destroy())
 
     viz.root.mainloop()
-    
-    
-
-    
